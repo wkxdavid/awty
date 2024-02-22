490f6508bc60fcda2df25b85060d0ef270832aea
wkxdavid <dpham722@uw.edu>
2024-02-21 21:52:37 -0800

    part 2 working?

--- app/src/main/java/edu/uw/ischool/dpham722/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/dpham722/awty/MainActivity.kt
@@ -6,14 +6,19 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.SEND_SMS
 import android.os.Bundle
 import android.telephony.PhoneNumberUtils
+import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import java.util.Locale
 
 const val ALARM_ACTION = "edu.uw.ischool.dpham722.ALARM"
@@ -22,6 +27,7 @@ const val MIN_IN_MILLIS = 60 * 1000
 
 class MainActivity : AppCompatActivity() {
     private var isAwtyActivated: Boolean = false
+
     private var receiver: BroadcastReceiver? = null
 
     private lateinit var messageEditText: EditText
@@ -54,19 +60,25 @@ class MainActivity : AppCompatActivity() {
         val intervalMillis = intervalMins.toLongOrNull()
 
         if (message.isNotEmpty() && phone.isNotEmpty() && intervalMillis?.let { it > 0 } == true) {
-            isAwtyActivated = true
-            updateUIForStart()
+            if (ContextCompat.checkSelfPermission(this, SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                isAwtyActivated = true
+                updateUIForStart()
 
-            val formattedPhone = PhoneNumberUtils.formatNumber(phone, Locale.getDefault().country)
-            scheduleRepeatingAlarm(formattedPhone, message, intervalMillis)
+                val formattedPhone = PhoneNumberUtils.formatNumber(phone, Locale.getDefault().country)
+                scheduleRepeatingAlarm(formattedPhone, message, intervalMillis)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(SEND_SMS), 1)
+            }
         } else {
-            Toast.makeText(this, "Oh noes, one or more values are invalid.", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Oh no! Cannot start AWTY because one or more values are invalid. Fix and try again.", Toast.LENGTH_LONG).show()
         }
     }
 
     private fun stopAwty() {
-        unregisterReceiver(receiver)
-        receiver = null
+        if (receiver != null) {
+            unregisterReceiver(receiver)
+            receiver = null
+        }
 
         isAwtyActivated = false
         updateUIForStop()
@@ -80,45 +92,28 @@ class MainActivity : AppCompatActivity() {
         startStopBtn.text = getString(R.string.start_button)
     }
 
-    private fun scheduleRepeatingAlarm(phone: String, message: String, intervalMillis: Long?) {
+    private fun scheduleRepeatingAlarm(phone: String, message: String, intervalMillis: Long) {
         val intent = Intent(ALARM_ACTION)
-        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
         val alarmManager: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.setRepeating(
             AlarmManager.RTC_WAKEUP,
             System.currentTimeMillis(),
-            intervalMillis!! * MIN_IN_MILLIS,
+            intervalMillis * MIN_IN_MILLIS,
             pendingIntent
         )
 
         if (receiver == null) {
-            receiver = createBroadcastReceiver(phone, message)
+            receiver = object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    Log.i(TAG, "Received request to send SMS to $phone")
+                    val smsManager = SmsManager.getDefault()
+                    smsManager.sendTextMessage(phone, null, message, null, null)
+                    Log.i(TAG, "Sent '$message' to $phone")
+                }
+            }
             val filter = IntentFilter(ALARM_ACTION)
             registerReceiver(receiver, filter)
         }
-    }
-
-    private fun createBroadcastReceiver(phone: String, message: String): BroadcastReceiver {
-        return object : BroadcastReceiver() {
-            override fun onReceive(context: Context?, intent: Intent?) {
-                Log.i(TAG, "received. making toast...")
-                showCustomToast(phone, message)
-            }
-        }
-    }
-
-    private fun showCustomToast(phone: String, message: String) {
-        val customToastView = layoutInflater.inflate(R.layout.custom_toast, null)
-        val toastCaptionTextView = customToastView.findViewById<TextView>(R.id.captionTextView)
-        val toastMessageTextView = customToastView.findViewById<TextView>(R.id.messageTextView)
-
-        toastCaptionTextView.text = getString(R.string.texting_phone, phone)
-        toastMessageTextView.text = message
-
-        val customToast = Toast(this).apply {
-            duration = Toast.LENGTH_LONG
-            view = customToastView
-        }
-        customToast.show()
     }
 }
